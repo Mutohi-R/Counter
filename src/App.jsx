ea1e8283d5e4a7968c0fc0c22a079b07d1ed9d94
Mutohir Okuniyi <mutohirokuniyi@gmail.com>
2023-09-03 15:11:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { ChevronDown } from 'react-feather'
+import { ChevronUp } from 'react-feather'
+import { ChevronsUp } from 'react-feather'
+import { ChevronsDown } from 'react-feather'
+import { Hash } from 'react-feather'
+import { RefreshCcw } from 'react-feather'
+import './App.css'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  const increaseByOne = () => {
+    return (setCount(count => count + 1))
+  }
+
+  const increaseByTen = () => {
+    return (setCount(count => count + 10))
+  }
+
+  const decreaseByOne = () => {
+    return (setCount(count => count - 1))
+  }
+
+  const decreaseByTen = () => {
+    return (setCount(count => count - 10))
+  }
+
+  const reset = () => {
+    return (setCount(0))
+  }
+
+  const random = () => {
+    return (setCount(Math.floor(Math.random() * 100)))
+  }
+
+  return (
+    <>
+      <div className="container">
+      <div className="countState">
+        <p className="state">Current Value:</p>
+        <p className="count">{count}</p>
+      </div>
+
+        <div className="buttonSet">
+          <button onClick={increaseByOne}><ChevronUp /></button>
+          <button onClick={increaseByTen}><ChevronsUp /></button>
+          <button onClick={reset}><RefreshCcw /></button>
+          <button onClick={random}><Hash /></button>
+          <button onClick={decreaseByTen}><ChevronsDown /></button>
+          <button onClick={decreaseByOne}><ChevronDown /></button>
+        </div>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
